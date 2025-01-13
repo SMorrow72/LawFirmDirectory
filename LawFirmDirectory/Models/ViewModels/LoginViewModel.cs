18cbd0e058474b260b3b0d66db9b22652c0f97bf
Sarah Morrow <smorrow72@gmail.com>
2025-01-13 18:12:51 -0500

    Added simple user authentication

---
+++ LawFirmDirectory/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LawFirmDirectory.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide a UserName")]
+        public string? UserName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide a Password")]
+        public string? Password { get; set; }
+    }
+}
