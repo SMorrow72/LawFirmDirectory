0f231f3c38c6f80c06c186fe8535dc74252977c3
Sarah Morrow <smorrow72@gmail.com>
2025-01-13 12:56:35 -0500

    Add project files.

---
+++ LawFirmDirectory/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LawFirmDirectory.Data
+{
+    public class AppDBContext : DbContext
+    {
+        string _connectionString = "data source=Nootbook;initial catalog=LawFirmDB;trusted_connection=true;TrustServerCertificate=True";
+
+        public DbSet<Attorney> Attorneys {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ LawFirmDirectory/Data/Attorney.cs
@@ -0,0 +1,11 @@
+﻿namespace LawFirmDirectory.Data
+{
+    public class Attorney
+    {
+        public int AttorneyId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
